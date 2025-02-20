e1f75325bf175eda938ef49a480da4f2d94cc547
Jhoan Artigas <johan851artigas@gmail.com>
2025-02-20 17:23:50 -0400

    Update function.py
    nombre de la condicion de color de ojos

--- function.py
+++ function.py
@@ -3,7 +3,7 @@ def suma(*num):
 
     
 print(suma(2,3,4,5))
-print("hola catalina ojos de colores diferentes")
+print("hola catalina ojos de colores diferentes, La heterocromía es una condición ocular que se caracteriza por tener los ojos de diferente color")
 print(suma(2,3,4,5,5,12))
 
-print("hola kari coromoto")
+print("hola kari coromoto ...")
